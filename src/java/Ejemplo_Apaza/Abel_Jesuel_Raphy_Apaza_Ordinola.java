90cd3025f1c6d715e60816b8f113521d63ef15a4
Jesuel <Jesuel@Jesuel-PC.mshome.net>
2018-04-09 15:30:12 -0500

    jesuel

--- src/java/Ejemplo_Apaza/Abel_Jesuel_Raphy_Apaza_Ordinola.java
+++ src/java/Ejemplo_Apaza/Abel_Jesuel_Raphy_Apaza_Ordinola.java
@@ -1,19 +1,26 @@
-
 package Ejemplo_Apaza;
 
 import javax.swing.JOptionPane;
 
 public class Abel_Jesuel_Raphy_Apaza_Ordinola {
+
     public static void main(String[] args) {
-        try {
-            double n = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el número del que desea obtener su raíz cuadrada."));
-            JOptionPane.showMessageDialog(null, "La raíz cuadrada de "+n+" es: "+calcularRaiz(n));
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Ingrese un caracter válido \n    "+e.getMessage());
+        boolean condicion = true;
+        double n = 0;
+        while (condicion) {
+            try {
+                 n = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el número del que desea obtener su raíz cuadrada."));
+                condicion = false;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Ingrese un caracter válido \n    " + e.getMessage());
+            }
+        }
+        if (!condicion) {
+            JOptionPane.showMessageDialog(null, "La raíz cuadrada de " + n + " es: " + calcularRaiz(n));
         }
     }
-    
-    public static double calcularRaiz(double n){
+
+    public static double calcularRaiz(double n) {
         return Math.sqrt(n);
     }
 }
