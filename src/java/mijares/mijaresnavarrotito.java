0af65c2e1939e3245ad111e98d3efd0af62af537
mijares123 <meniannavarro@upeu.edu.pe>
2018-04-09 16:21:03 -0500

    dqowudbaljdqwdawdawdwd

---
+++ src/java/mijares/mijaresnavarrotito.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mijares;
+
+/**
+ *
+ * @author mijares
+ */
+public class mijaresnavarrotito {
+    public static void main(String[] args) {
+        System.out.println("nwdainelae");
+        System.out.println("nel");
+    }
+}
